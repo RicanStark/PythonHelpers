b045e2337cb034466bf6e667571b2d32f32860e7
RicanStark <nils-vb@gmx.de>
2024-06-16 00:44:01 +0200

    program now continues

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,12 +1,13 @@
 # rounding-capable weight converter:
-weight = float(input("Weight: "))
-unitOfMeasurement = input("(K)g or (L)bs: ")
-if unitOfMeasurement.upper() == "K":
-    print("Weight in Lbs: " + str(float(weight / 0.45).__round__(2)))
-elif unitOfMeasurement.upper() == "L":
-    print("Weight in Kgs: " + str(float(weight * 0.45).__round__(2)))
-else:
-    print("Not a valid unit of measurement")
+while True:
+    weight = float(input("Weight: "))
+    unitOfMeasurement = input("(K)g or (L)bs: ")
+    if unitOfMeasurement.upper() == "K":
+        print("Weight in Lbs: " + str(float(weight / 0.45).__round__(2)))
+    elif unitOfMeasurement.upper() == "L":
+        print("Weight in Kgs: " + str(float(weight * 0.45).__round__(2)))
+    else:
+        print("Not a valid unit of measurement")
 print("Done")
 
-# to do: make the program continue to run after last input; add input option to terminate program
+# to do: add input option to terminate program (in both phases)
