97bd82cc0ec93821b3433c1feaaf78f869f9a038
RicanStark <nils-vb@gmx.de>
2024-06-16 19:45:58 +0200

    oh no, it's not foolproof. I'm a fool!

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,25 +1,32 @@
 # rounding-capable weight converter:
+roundCount = 0
 while True:
-    userInput1 = input("Weight: ")
-    if userInput1.lower() == "help":
-        print("Type a number as weight. Type 'quit' to exit.")
-    elif userInput1.lower() == "quit":
-        break
-    elif type(float(userInput1)) != float:
-        print("Not a valid value for 'Weight'. Type 'help' to display commands.")
+    if roundCount == 0:
+        print("Please for the love of all that is holy either type 'help', 'quit' or a number, or you will bring down doom on us all!")
+        roundCount += 1
     else:
-        weight = float(userInput1)
-        userInput2 = input("(K)g or (L)bs: ")
-        if userInput2 == "help":
-            print("Type 'K' to set the Unit of Measurement as Kg and convert to Lbs;\nType 'L' to set the Unit of measurement as Lbs and convert to Kg.\nType 'quit' to exit.")
-        elif userInput2 == "quit":
+        userInput1 = input("Weight: ")
+        if userInput1.lower() == "help":
+            print("Type a number as weight. Type 'quit' to exit.")
+        elif userInput1.lower() == "quit":
             break
+        elif type(float(userInput1)) != float:
+            print("Not a valid value for 'Weight'. Type 'help' to display commands.")
         else:
-            unitOfMeasurement = userInput2.upper()
-            if unitOfMeasurement == "K":
-                print("Weight in Lbs: " + str(float(weight / 0.45).__round__(2)))
-            elif unitOfMeasurement == "L":
-                print("Weight in Kgs: " + str(float(weight * 0.45).__round__(2)))
+            weight = float(userInput1)
+            userInput2 = input("(K)g or (L)bs: ")
+            if userInput2 == "help":
+                print("Type 'K' to set the Unit of Measurement as Kg and convert to Lbs;\nType 'L' to set the Unit of measurement as Lbs and convert to Kg.\nType 'quit' to exit.")
+            elif userInput2 == "quit":
+                break
             else:
-                print("Not a valid unit of measurement")
+                unitOfMeasurement = userInput2.upper()
+                if unitOfMeasurement == "K":
+                    print("Weight in Lbs: " + str(float(weight / 0.45).__round__(2)))
+                elif unitOfMeasurement == "L":
+                    print("Weight in Kgs: " + str(float(weight * 0.45).__round__(2)))
+                else:
+                    print("Not a valid unit of measurement")
 print("Done")
+
+# to do: make it so that typing 'help' in the second input leaves you in that part of the program and doesn't take you back to the start (use functions?)
