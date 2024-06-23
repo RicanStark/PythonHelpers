d9c436c54ab0bee455ca1ae6cb153b6297834c45
RicanStark <nils-vb@gmx.de>
2024-06-23 11:51:57 +0200

    Slightly optimized/corrected nomenclature

--- BildSortieromat3000.py
+++ BildSortieromat3000.py
@@ -1,12 +1,13 @@
+# program capable of renaming all files of the same type within a folder in ascending order:
 import os, sys
-
-# here goes file path:
+# insert file path here:
 filepath = "/Users/nilsvanburk/Pictures/Bildschirmfotos/PythonTest"
-platzhalter = "placeholder"
-# here goes file extension:
+platzhalter = "Bildschirmfoto"
+# filter by file extension here:
 fileextension = ".png"
 
 filelist=sorted(os.listdir(filepath))
+# optionally check the sorted list:
 # print(filelist)
 
 temp_filelist=[]
@@ -16,13 +17,15 @@ for element in filelist:
         temp_filelist.append(element)
 
 if len(filelist) >999:
-    print("WARNING! More than 999 files in "+filepath+"!")
+    print("WARNING! More than 999 files in " + filepath + "!")
     sys.exit()
 
 for i in range(len(temp_filelist)):
-    # print(platzhalter+"_"+"{:03d}".format(i+1))
-    file=filepath+"/"+temp_filelist[i]
-    # here goes: instead of "platzhalter" new filename / beachte: # {:04d} for 1000-9999 files!
-    newfile=filepath+"/"+platzhalter+"_"+"{:03d}".format(i+1)+fileextension
+    # optionally check the renaming scheme first:
+    # print(platzhalter + "_" + "{:03d}".format(i+1))
+    file = filepath + "/" + temp_filelist[i]
+    # note: {:04d} for 1000-9999 files!
+    newfile = filepath + "/" + platzhalter + "_" + "{:03d}".format(i+1)+fileextension
+    # optionally check the renamed file list before actually renaming:
     # print(file)
     os.rename(file, newfile)
